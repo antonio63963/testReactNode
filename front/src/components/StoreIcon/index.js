cd959b09e11cd033e55b85978dc5c0a06ddce79e
unknown <shotvideo25@gmail.com>
2022-01-01 21:49:29 +0200

    show general amount

--- front/src/components/StoreIcon/index.js
+++ front/src/components/StoreIcon/index.js
@@ -1,12 +1,12 @@
 
-import { useSelect, useDispatch } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { Badge, Avatar } from 'antd';
 import style from './storeIcon.module.css';
 const StoreIcon = () => {
-  
+  const generalAmount = useSelector(state => state.store.generalAmount)
   return (
   
-    <Badge className={style.badge} count={99} overflowCount={10}>
+    <Badge className={style.badge} count={generalAmount} overflowCount={10}>
       <i className="fas fa-shopping-basket"></i>
       {/* <Avatar shape="square" size="large" /> */}
     </Badge>
